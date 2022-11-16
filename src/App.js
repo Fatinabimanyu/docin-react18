8bdace169862f0fb715b8f670011313506b43f5f
Muhammad Haidar Izzuddin <muhammadhaidar@mail.ugm.ac.id>
2022-11-16 23:27:45 +0700

    feat: add sign up page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route  } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
+import SignUp from "./components/SignUp";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/signup" element={<SignUp />} />
       </Routes>
     </Router>
   );
