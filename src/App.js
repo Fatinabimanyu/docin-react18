bd44c40a200df9f4fe378cb64ad4184cde6817c1
dindasabela <dindasabela02@mail.ugm.ac.id>
2022-12-09 15:32:26 +0700

    feat: add private route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import UserDashboard from "./pages/UserDashboard";
 import SignUpUser from "./pages/UserSignUp";
@@ -12,27 +17,54 @@ import PageNotFound from "./pages/PageNotFound";
 import DoctorDetail from "./pages/DoctorDetail";
 import UserEditProfile from "./pages/UserEditProfile";
 
+const PrivateRoute = ({ auth: { isAuthenticated }, children }) => {
+  return isAuthenticated ? children : <Navigate to="/login-doctor" />;
+};
+
+const PrivateRouteD = ({ auth: { isAuthenticated }, children }) => {
+  return isAuthenticated ? children : <Navigate to="/login-doctor" />;
+};
+
 function App() {
   return (
     <>
-    <Router>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/signup-user" element={<SignUpUser />} />
-        <Route path="/login-user" element={<LogInUser />} />
-        <Route path="/signup-doctor" element={<SignUpDoctor />} />
-        <Route path="/login-doctor" element={<LogInDoctor />} />
-        <Route path="/find-doctor" element={<FindDoctor />} />
-        <Route path="/doctor-dashboard" element={<DoctorDashboard />} />
-        <Route path="/user-dashboard" element={<UserDashboard />} />
-        <Route path="/user-edit-profile" element={<UserEditProfile />} />
-        <Route path="/doctor/:id" element={<DoctorDetail />} />
-        <Route path="/*" element={<PageNotFound />} />
-      </Routes>
-    </Router>
-    <ToastContainer />
+      <Router>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/signup-user" element={<SignUpUser />} />
+          <Route path="/login-user" element={<LogInUser />} />
+          <Route path="/signup-doctor" element={<SignUpDoctor />} />
+          <Route path="/login-doctor" element={<LogInDoctor />} />
+          <Route path="/find-doctor" element={<FindDoctor />} />
+          <Route
+            path="/doctor-dashboard"
+            element={
+              <PrivateRouteD auth={{ isAuthenticated: false }}>
+                <DoctorDashboard />
+              </PrivateRouteD>
+            }
+          />
+          <Route
+            path="/user-dashboard"
+            element={
+              <PrivateRoute auth={{ isAuthenticated: false }}>
+                <UserDashboard />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/doctor/:id"
+            element={
+              <PrivateRoute auth={{ isAuthenticated: false }}>
+                <DoctorDetail />
+              </PrivateRoute>
+            }
+          />
+          <Route path="/*" element={<PageNotFound />} />
+        </Routes>
+      </Router>
+      <ToastContainer />
     </>
   );
 }
-
 export default App;
